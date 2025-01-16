ece62d3d4166e6d630919949fae48a6e080bf031
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2025-01-16 12:06:20 +0000

    Add Hero Section

---
+++ react_02/src/components/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return(
+        <div className="font-poppins max-w-1.5xl mx-auto pt-18">
+            <h1 className="font-extrabold text-5xl h-18">CONTACT US</h1>
+            <p className="font-semibold text-base text-para-gray h-18 max-w-para-w">LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA. </p>
+        </div>
+    );
+}
+
+export default Hero;
--- react_02/tailwind.config.js
+++ react_02/tailwind.config.js
@@ -6,6 +6,16 @@ export default {
     extend: {
       colors:{
         "txt-color": "#2E2E2E",
+          "btn-red": "#D01C28",
+          "para-gray": "#5A5959",
+      },
+      spacing: {
+        '18': '4.5rem',
+      },
+      maxWidth: {
+        "1.5xl": "72.563rem",
+        "para-w": "60.938rem",
+        mid: "25.25rem",
       },
       fontFamily: {
         poppins: ["Poppins"],
