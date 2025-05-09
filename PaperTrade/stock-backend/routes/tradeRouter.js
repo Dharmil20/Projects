85980ef3f681ae4a5699ce6490be57b8cdf2ca4f
Dharmil20 <80124029+Dharmil20@users.noreply.github.com>
2025-05-09 21:48:26 +0530

    Update tradeRouter.js

--- PaperTrade/stock-backend/routes/tradeRouter.js
+++ PaperTrade/stock-backend/routes/tradeRouter.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { buyStock, sellStock } = require("../controllers/tradeController");
-const tradeValidation = require("../middlewares/tradeValidation"); // Use the new validation
+const tradeValidation = require("../middlewares/tradeValidation");
 const tradeRouter = express.Router();
 
-tradeRouter.post("/buy", tradeValidation, buyStock); // Apply trade validation
-tradeRouter.post("/sell", tradeValidation, sellStock); // Apply trade validation
+tradeRouter.post("/buy", tradeValidation, buyStock);
+tradeRouter.post("/sell", tradeValidation, sellStock);
 
 module.exports = tradeRouter;
